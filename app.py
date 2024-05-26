2b1fae370dec9dba73aca7f9678f44e6000bc974
SayQusay <qusayakib24@gmail.com>
2024-05-26 20:44:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,4 @@ import numpy as np
 st.write("Nama Saya Qusay")
 
 #Header Section
-def header_section():
-  st.markdown("<h1 style='text-align: center; color:#F7F8F7;'>Your heart's rhythm reveals more than just beats</h1> <h4 style='text-align: center; color: #555;'>Unlock the SECRETS of your heart's rhythm </h4>",unsafe_allow_html=True)  
+st.markdown("<h1 style='text-align: center; color:#F7F8F7;'>Your heart's rhythm reveals more than just beats</h1> <h4 style='text-align: center; color: #555;'>Unlock the SECRETS of your heart's rhythm </h4>",unsafe_allow_html=True)  
