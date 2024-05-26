d7a913a239262653251ac41f04cc8f69b7fbe9e7
SayQusay <qusayakib24@gmail.com>
2024-05-26 21:02:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,5 @@ from st_click_detector import click_detector
 content = """
     <a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
     """
-clicked = click_detector(content)
 
-st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
+st.markdown(content,unsafe_allow_html=True)
