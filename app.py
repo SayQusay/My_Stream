afa740a0d93ea38c1869c1bd100356919faad9d2
SayQusay <qusayakib24@gmail.com>
2024-05-26 21:02:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from st_click_detector import click_detector
 
 content = """
-    <a href='#' id='Image 1'><img width='20%' src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
+    <a href='#' id='Image 1'><src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
     """
 
 st.markdown(content,unsafe_allow_html=True)
