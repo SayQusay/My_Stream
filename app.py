a11c6acd6d8c020c3b83f485c6b3ae535df2e501
SayQusay <qusayakib24@gmail.com>
2024-05-27 14:46:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ page_names_to_func = {
     "HRV Analysis":page4,
 }
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_func.keys())
+page_names_to_func[selected_page]()
 
