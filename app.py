02e70d685110423b6062114ff4cfe001a3f04d6c
SayQusay <qusayakib24@gmail.com>
2024-05-27 14:02:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 import streamlit as st
 import pandas as pd
-import plost
 import numpy as np
-from st_click_detector import click_detector
 
-content = """
-    <a href='#' id='Image 1'><img src='https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=200'></a>
-    """
+def main_page():
+    st.markdown("# Main page")
+    st.sidebar.markdown("Main Page")
 
-st.markdown(content,unsafe_allow_html=True)
+def page2():
+    st.markdown(" Data")
+    st.sidebar.markdown("Data")
+
+def page3():
+    st.markdown("RR Detection")
+    st.sidebar.markdown("RR Detection")
+
+def page4():
+    st.markdown("HRV Analysis")
+    st.sidebar.markdown("HRV Analysis")
+
+page_names_to_func = {
+    "Main Page": main_page,
+    "Data":page2,
+    "RR Detection":page3,
+    "HRV Analysis":page4,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
+
